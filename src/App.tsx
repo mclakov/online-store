15f9d3f655b2ae0ca5cf184df73e0c2064f80320
Roman Maklakov <mclakov@gmail.com>
2023-01-07 18:21:00 +0300

    feat: header done

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,39 @@ import { AppLib } from './lib/appLib';
 import { productsData } from './lib/data/products';
 import { prodData } from './globalTypes';
 import Page404 from './components/Page404';
+import Cart from './components/Cart';
+import ProductDetails from './components/ProductDetails';
 
 const appLib = new AppLib(productsData);
 
 function App() {
     const [products, setProducts] = useState(appLib.getProductsData());
+    const [prodDetails, setProdDetails] = useState(products[0]);
+    const [prodsInCart, setProdsInCart] = useState([]);
+    const [cartPriceTotal, setCartPriceTotal] = useState(0);
+    const [cartAmountTotal, setCartAmountTotal] = useState(0);
 
 
     return (
         <div className='App'>
-            <Header />
+            <Header
+                cartPriceTotal={cartPriceTotal}
+                cartAmountTotal={cartAmountTotal}
+            />
             <Router>
                 <Routes>
                     <Route path='/store' element={<Main
                         products={products}
                     />}>
                     </Route>
-                    <Route path='/cart' element={<Main
+                    <Route path='/' element={<Navigate replace to='/store' />}>
+                    </Route>
+                    <Route path='/cart' element={<Cart
                         products={products}
                     />}>
                     </Route>
-                    <Route path='/product-details' element={<Main
-                        products={products}
+                    <Route path='/product-details' element={<ProductDetails
+                        prod={prodDetails}
                     />}>
                     </Route>
                     <Route path='/404' element={<Page404 />}>
