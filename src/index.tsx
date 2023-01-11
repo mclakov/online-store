290e0a9f0a3932f9a60218496c343312275498a9
Roman Maklakov <mclakov@gmail.com>
2023-01-11 07:10:53 +0300

    fix: package.json

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,16 @@ import './assets/styles/Header.scss';
 import './assets/styles/Filters.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
           <App />
-      </BrowserRouter>
+      </HashRouter>
   </React.StrictMode>
 );
 
